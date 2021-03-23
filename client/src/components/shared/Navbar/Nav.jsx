d9ac79f69f9c8deb0c152658e1fb64a5ef48ba45
Daniel Michael <itzdanielmichael@gmail.com>
2021-03-23 00:52:35 -0400

    update Nav.jsx

--- client/src/components/shared/Navbar/Nav.jsx
+++ client/src/components/shared/Navbar/Nav.jsx
@@ -1,30 +1,59 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const StyledNav = styled.nav`
-  position: sticky;
+  position: fixed;
   top: 0;
-
+  width: 100%;
+  padding: 20px;
+  height: 30px;
   display: flex;
+  justify-content: space-between;
+  z-index: 1;
+  background-color: ${({ isShowing }) => isShowing && '#111'};
 
   .nav__logo {
+    position: fixed;
+    left: 20px;
     width: 80px;
+    object-fit: contain;
+  }
+
+  .nav__avatar {
+    position: fixed;
+    right: 20px;
+    width: 30px;
     object-fit: contain;
   }
 `;
 
-const Nav = () => (
-  <StyledNav aria-label="navbar">
-    <img
-      className="nav__logo"
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1280px-Netflix_2015_logo.svg.png"
-      alt="Netflix Logo"
-    />
-    <img
-      className="nav__avatar"
-      src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-      alt="Profile Pic"
-    />
-  </StyledNav>
-);
+export default function Nav() {
+  const [isBackgroundShowing, setIsBackgroundShowing] = useState(false);
 
-export default Nav;
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        setIsBackgroundShowing(true);
+      } else setIsBackgroundShowing(false);
+    });
+
+    return () => {
+      window.removeEventListener('scroll');
+    };
+  }, []);
+
+  return (
+    <StyledNav aria-label="navbar" isShowing={isBackgroundShowing}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1280px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Profile Pic"
+      />
+    </StyledNav>
+  );
+}
