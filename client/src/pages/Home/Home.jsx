ce13a81ee4390ce1cdcee8bc3f06d6e56ab761df
Daniel <itzdanielmichael@gmail.com>
2021-03-24 20:41:05 -0400

    update Home.sx

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -5,11 +5,14 @@ import Banner from '../../components/MovieComponents/Banner/Banner';
 import MovieCard from '../../components/MovieComponents/MovieCard/MovieCard';
 import Row from '../../components/MovieComponents/Row/Row';
 import Layout from '../../components/shared/Layout/Layout';
-import { TMDB_API } from '../../services/apiConfig';
-import { InnerColumn } from './home.styles';
 
-// utils
+// utils, helpers and Services
 import { movieRows } from './home.utils';
+import { TMDB_API } from '../../services/apiConfig';
+import Levenshtein from 'levenshtein';
+
+//  styles
+import { InnerColumn } from './home.styles';
 
 const baseUrl = 'https://image.tmdb.org/t/p/original';
 
@@ -29,10 +32,16 @@ function Home() {
     setQueriedMovies(data.results);
   };
 
-  const getQueriedMovies = () =>
-    queriedMovies.filter((movie) =>
-      movie.title.toLowerCase().includes(search.toLowerCase())
-    );
+  const getQueriedMovies = () => {
+    // https://stackoverflow.com/questions/26796276/array-sort-on-the-basis-of-search-text-matching-using-pure-javascript
+    // https://github.com/gf3/Levenshtein
+    // https://en.wikipedia.org/wiki/Levenshtein_distance
+    return queriedMovies.sort((a, b) => {
+      var leva = new Levenshtein(a.title, search).distance; // the movie that is closest to user's search input will appear in top-left.
+      var levb = new Levenshtein(b.title, search).distance;
+      return leva - levb;
+    });
+  };
 
   const ROWS = Children.toArray(
     movieRows.map(({ title, fetchUrl }) => (
