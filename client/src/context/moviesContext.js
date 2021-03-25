fde9ab4607b2b42eb90bf09b4a5b44e32cc1732a
Daniel <itzdanielmichael@gmail.com>
2021-03-25 16:47:18 -0400

    update MoviePlayBackView.jsx

---
+++ client/src/context/moviesContext.js
@@ -0,0 +1,53 @@
+import React, { createContext, useReducer, useMemo } from 'react';
+import { movieRows } from '../pages/Home/home.utils';
+import { getAllMovies } from '../services/movies';
+export const MoviesStateContext = createContext();
+export const MoviesDispatchContext = createContext();
+
+const movieReducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH':
+      return {
+        allMovies: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const MoviesContextProvider = ({ children }) => {
+  const initialMoviesState = {
+    allMovies: [],
+    moviesAreLoading: true,
+  };
+
+  const [state, dispatch] = useReducer(movieReducer, initialMoviesState);
+
+  useMemo(async () => {
+    let fetchUrls = [];
+    movieRows.forEach(({ fetchUrl }) => {
+      fetchUrls.push(fetchUrl);
+    });
+
+    fetchUrls.map(
+      async (url) =>
+        await getAllMovies(url).then((movieData) =>
+          dispatch({
+            type: 'FETCH',
+            payload: movieData,
+            moviesAreLoading: false,
+          })
+        )
+    );
+  }, []);
+
+  return (
+    <MoviesStateContext.Provider value={state}>
+      <MoviesDispatchContext.Provider value={dispatch}>
+        {children}
+      </MoviesDispatchContext.Provider>
+    </MoviesStateContext.Provider>
+  );
+};
+
+export default MoviesContextProvider;
--- client/src/pages/MoviePages/MoviePlaybackView.jsx
+++ client/src/pages/MoviePages/MoviePlaybackView.jsx
@@ -1,26 +1,43 @@
-import { useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getAllMovies } from '../../services/movies';
-import { movieRows } from '../Home/home.utils';
+import { MoviesStateContext } from '../../context/moviesContext';
 
-function MoviePlaybackView({ location: { state } }) {
-  const { id, trailerUrl } = useParams();
+export default function MoviePlaybackView({ location: { state } }) {
+  const params = useParams();
 
-  let fetchUrls = [];
-  movieRows.forEach(({ fetchUrl }) => {
-    fetchUrls.push(fetchUrl);
-  });
+  const [videoData, setVideoData] = useState({});
+
+  const { allMovies } = useContext(MoviesStateContext);
 
   useEffect(() => {
-    fetchUrls.map(
-      async (url) =>
-        await getAllMovies(url).then((data) => console.log({ data }))
+    if (state !== undefined) {
+      // if user clicks from interfacce and doesn't type the url:
+      return setVideoData({
+        movie: state.movie,
+        trailerUrl: state.trailerUrl,
+      });
+    }
+
+    //  else if the user decides to write the id in the url bar instead of clicking the movie poster in the actual app.
+    //  find the object properties by matching the id to the id from useParams.
+    const foundMovie = allMovies.find(
+      (movie) => movie.id === Number(params.id)
     );
-  });
+    return setVideoData({
+      movie: foundMovie,
+      trailerUrl: params.trailerUrl,
+    });
+
+    // the reason why I want to check for both use-cases instead of just going with useParams only is because:
+    // if the user clicks from the interface the video going to load noticeably faster, so I want to leave that as an option.
+    // and I can't just go with only props because:
+    // if the user types the url without clicking on the interface, it'll throw an error (the props don't exist).
+  }, [allMovies, params, state]);
+
+  const { movie, trailerUrl } = videoData;
 
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
-      {/* <YouTube videoId={trailerUrl} opts={VIDEO_OPTIONS} /> */}
       <iframe
         frameBorder="0"
         allowFullScreen="1"
@@ -29,7 +46,7 @@ function MoviePlaybackView({ location: { state } }) {
         width="100vw"
         height="100vh"
         src={`https://www.youtube.com/embed/${trailerUrl}?autoplay=1&amp;enablejsapi=1&amp;widgetid=5&showinfo=0`}
-        id={id}
+        id={movie?.id}
         style={{
           width: '100%',
           height: '100%',
@@ -38,5 +55,3 @@ function MoviePlaybackView({ location: { state } }) {
     </div>
   );
 }
-
-export default MoviePlaybackView;
