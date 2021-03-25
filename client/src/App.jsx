0c0dc3292e6ac40d1b647f68fc4413ec07a0e30b
Daniel <itzdanielmichael@gmail.com>
2021-03-25 11:43:05 -0400

    update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css';
 import AppRouter from './router/AppRouter';
 
-function App() {
-  return (
-    <div className="App">
-      <AppRouter />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <AppRouter />
+  </div>
+);
 
 export default App;
