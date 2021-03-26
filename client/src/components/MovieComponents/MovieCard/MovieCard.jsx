54a769d6abc5e20b677613f43f6d0324ad4b3680
Daniel <itzdanielmichael@gmail.com>
2021-03-26 12:15:40 -0400

    update MovieCard

--- client/src/components/MovieComponents/MovieCard/MovieCard.jsx
+++ client/src/components/MovieComponents/MovieCard/MovieCard.jsx
@@ -2,14 +2,13 @@
 // I could just put the styling here with styled-components, but what if in the future we want to reuse it in a different page?
 // therefore we give it a flexible className as props.
 
-const MovieCard = ({ src, alt, className, onClick }) =>
-  !src.includes('null') && (
-    <img
-      src={src}
-      alt={alt}
-      className={className ?? 'movie-card'}
-      onClick={onClick}
-    />
-  );
+const MovieCard = ({ src, alt, className, onClick }) => (
+  <img
+    src={src}
+    alt={alt}
+    className={className ?? 'movie-card'}
+    onClick={onClick}
+  />
+);
 
 export default MovieCard;
