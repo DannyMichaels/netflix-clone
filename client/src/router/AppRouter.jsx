d6f651a6349ac0523b1d800504b72adfc9c333cd
Daniel <itzdanielmichael@gmail.com>
2021-04-09 13:23:34 -0400

    update AppRouter.jsx, PrivateRoute.js

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -10,18 +10,22 @@ import MoviePlaybackView from '../pages/MoviePages/MoviePlaybackView';
 import BrowseByCategoryAndId from '../pages/Browse/ByCategoryAndId/BrowseByCategoryAndId';
 import ProfileSelect from '../pages/Profiles/ProfileSelect/ProfileSelect';
 import ProfileManage from '../pages/Profiles/ProfileManage/ProfileManage';
+import PrivateRoute from './PrivateRoute';
 
 const AppRouter = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path={ROUTES.SELECT_PROFILE} component={ProfileSelect} />
       <Route exact path={ROUTES.MANAGE_PROFILE} component={ProfileManage} />
-      <Route exact path={ROUTES.BROWSE_ALL} component={Home} />
-      <Route
+      <PrivateRoute exact path={ROUTES.BROWSE_ALL} component={Home} />
+      <PrivateRoute
         path={[ROUTES.BROWSE_BY_GENRE, ROUTES.BROWSE_BY_PERSON]}
         component={BrowseByCategoryAndId}
       />
-      <Route path={ROUTES.MOVIE_PLAYBACK} component={MoviePlaybackView} />
+      <PrivateRoute
+        path={ROUTES.MOVIE_PLAYBACK}
+        component={MoviePlaybackView}
+      />
     </Switch>
   </BrowserRouter>
 );
--- client/src/router/PrivateRoute.js
+++ client/src/router/PrivateRoute.js
@@ -1,15 +1,16 @@
-import React from 'react';
 import { Redirect, Route } from 'react-router';
 import { ROUTES } from '../utils/navigation';
 
 export default function PrivateRoute({ component: Component, ...rest }) {
   const selectedProfile = localStorage.getItem('selectedProfile');
 
+  console.log({ selectedProfile });
+
   return (
     <Route
       {...rest}
       render={(props) => {
-        return selectedProfile ? (
+        return selectedProfile !== 'null' ? (
           <Component {...props} />
         ) : (
           <Redirect to={ROUTES.SELECT_PROFILE} />
