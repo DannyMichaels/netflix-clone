37236b0e06f5e565bdcefbd708ed69cdac869640
Daniel <itzdanielmichael@gmail.com>
2021-04-18 10:44:25 -0400

    update Row.jsx

--- client/src/components/MovieComponents/Row/Row.jsx
+++ client/src/components/MovieComponents/Row/Row.jsx
@@ -16,6 +16,14 @@ import ArrowForwardIcon from '@material-ui/icons/ArrowForwardIos';
 
 export default function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  // const [isVisible, setIsVisible] = useState(5);
+  // const [counter, setCounter] = useState(0);
+
+  const [visiblePosterCount, setVisiblePosterCount] = useState(
+    Math.round(document.body.clientWidth / 200)
+  );
+
+  // console.log(Math.round(document.body.clientWidth / 200));
 
   const rowRef = useRef(null);
 
@@ -27,40 +35,74 @@ export default function Row({ title, fetchUrl, isLargeRow }) {
     setMovies(moviesThatHaveImage);
   }, [fetchUrl]);
 
-  const onNavigate = (direction) => {
-    const elementToScroll = rowRef.current.querySelector('.row__posters');
-    const allPosters = rowRef.current.querySelectorAll('.movie__card--parent');
-    let currentScrollPosition = elementToScroll.scrollLeft;
-    let availableWidth = document.body.clientWidth;
-    let posterWidth = allPosters[0].clientWidth;
-    let visibleRange = currentScrollPosition + availableWidth;
-    let lastVisiblePoster;
-    let index = 0;
-    for (index; index < allPosters.length; index++) {
-      if (allPosters[index].offsetLeft + posterWidth >= visibleRange) {
-        lastVisiblePoster = allPosters[index];
-        break;
-      }
-    }
-    if (!lastVisiblePoster && allPosters.length > 0) {
-      lastVisiblePoster = allPosters[allPosters.length - 1];
-    }
+  const onScrollNext = () => {
+    // const visibleElements = rowRef.current.querySelector('.visible');
 
-    if (lastVisiblePoster) {
-      let scrollDistance =
-        lastVisiblePoster.offsetLeft + posterWidth - elementToScroll.scrollLeft;
-      elementToScroll.scrollTo({
-        top: 0,
-        left:
-          direction === 'forward'
-            ? elementToScroll.scrollLeft + scrollDistance
-            : elementToScroll.scrollLeft - scrollDistance,
-        behavior: 'smooth',
-      });
+    const allPosters = rowRef.current.querySelectorAll('.row__poster');
+
+    for (let i = 0; i < allPosters.length; i++) {
+      console.log('in i loop');
+      if (allPosters[i].classList.contains('visibile')) {
+        if (
+          i + 1 < allPosters.length &&
+          allPosters[i + 1].classList.contains('visibile')
+        ) {
+          console.log('in if block');
+          allPosters[i].classList.remove('visibile');
+        } else {
+          console.log('in else block');
+
+          for (
+            let j = 0;
+            j < visiblePosterCount && i + j < allPosters.length;
+            j++
+          ) {
+            console.log('in J for loop', { i, j });
+            allPosters[i + j].classList.add('visibile');
+          }
+
+          allPosters[i].scrollIntoView({
+            behavior: 'smooth',
+            inline: 'center',
+          });
+          break;
+        }
+      }
     }
   };
 
-  const CARDS = movies?.map((movie) => (
+  const onScrollBack = () => {
+    // const visibleElements = rowRef.current.querySelector('.visible');
+    const allPosters = rowRef.current.querySelectorAll('.row__poster');
+
+    for (let i = allPosters.length - 1; i >= 0; i--) {
+      console.log('in i loop');
+      if (allPosters[i].classList.contains('visibile')) {
+        if (
+          i - 1 < allPosters.length &&
+          allPosters[i - 1].classList.contains('visibile')
+        ) {
+          console.log('in if block');
+          allPosters[i].classList.remove('visibile');
+        } else {
+          console.log('in else block');
+          let j = 0;
+          for (j; j < visiblePosterCount && i - j > 0; j++) {
+            console.log('in J for loop', { i, j });
+            allPosters[i - j].classList.add('visibile');
+          }
+
+          allPosters[i - j].scrollIntoView({
+            behavior: 'smooth',
+            inline: 'center',
+          });
+          break;
+        }
+      }
+    }
+  };
+
+  const CARDS = movies?.map((movie, index) => (
     <MovieCard
       movie={movie}
       src={`${baseImgUrl}${
@@ -69,17 +111,16 @@ export default function Row({ title, fetchUrl, isLargeRow }) {
       isLargeRow={isLargeRow}
       alt={movie.name}
       key={movie.id}
-      className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+      className={`row__poster ${isLargeRow && 'row__posterLarge'} ${
+        index < visiblePosterCount ? 'visibile' : ''
+      }`}
     />
   ));
 
   return (
     <StyledRow aria-label="movies row" ref={rowRef}>
       <h2 className="row__title">{title}</h2>
-      <button
-        className="slider__nav prev"
-        onClick={() => onNavigate('backward')}
-      >
+      <button className="slider__nav prev" onClick={onScrollBack}>
         <span className="icon">
           {/* &lt; */}
           <ArrowBackIcon />
@@ -87,10 +128,7 @@ export default function Row({ title, fetchUrl, isLargeRow }) {
         {/* <span className="row__gradient" /> */}
       </button>
       <div className="row__posters">{CARDS}</div>
-      <button
-        className="slider__nav next"
-        onClick={() => onNavigate('forward')}
-      >
+      <button className="slider__nav next" onClick={onScrollNext}>
         <span className="icon">
           {/* &gt; */}
           <ArrowForwardIcon />
