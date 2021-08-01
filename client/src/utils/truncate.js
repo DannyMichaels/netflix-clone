91ccb2b7120a82e92d1cd876784ba402d11b3422
Daniel <itzdanielmichael@gmail.com>
2021-07-31 23:36:06 -0400

    update truncate.js

--- client/src/utils/truncate.js
+++ client/src/utils/truncate.js
@@ -1,3 +1,9 @@
+/**
+ * @method truncate
+ * @param {String} str
+ * @param {Number} n
+ * @return {Result}
+ */
 export const truncate = (str, n) => {
   if (typeof str === 'string') {
     return str?.length > n ? `${str?.substr(0, n - 1)} ...` : str ?? '';
