c81416845309df2eed338933e5f105fd30e366f7
Daniel <itzdanielmichael@gmail.com>
2021-09-26 15:29:43 -0400

    update Row.jsx

--- client/src/components/MovieComponents/Row/Row.jsx
+++ client/src/components/MovieComponents/Row/Row.jsx
@@ -86,13 +86,15 @@ export default function Row({ title, fetchUrl, isLargeRow, rowIndex }) {
       setUnclonedMoviesCount(moviesThatHaveImage.length);
 
       setMovies(moviesThatHaveImage);
-      setMoviesLoaded(rowIndex);
+      setMoviesLoaded(true);
     };
     fetchData();
-  }, [fetchUrl, currentProfile?.isKid, rowIndex]);
+  }, [fetchUrl, currentProfile?.isKid]);
 
   useLayoutEffect(() => {
-    if (moviesLoaded === rowIndex) {
+    // create clones of movies after dom has painted
+    // check if movies loaded
+    if (moviesLoaded) {
       const previousMoviesState = [...movies];
       const newMoviesState = [...movies];
 
@@ -128,14 +130,14 @@ export default function Row({ title, fetchUrl, isLargeRow, rowIndex }) {
       setTranslateXValue(-visiblePosterAmount * posterWideness);
       setMoviesUpdated(true);
       setMovies(newMoviesState);
-
-      console.log('UPDATED!');
     }
-  }, [moviesLoaded, rowIndex]);
+  }, [moviesLoaded]);
 
   useEffect(() => {
     if (skipTransition) {
       setTimeout(() => {
+        // this.animating = false;
+
         setSkipTransition(false);
       }, 10);
     }
@@ -161,6 +163,7 @@ export default function Row({ title, fetchUrl, isLargeRow, rowIndex }) {
     let visiblePosterAmount = Math.round(
       rowRef?.current?.clientWidth / posterWidth
     );
+    console.log({ visiblePosterAmount });
     setVisiblePosterCount(visiblePosterAmount);
   });
 
@@ -169,7 +172,7 @@ export default function Row({ title, fetchUrl, isLargeRow, rowIndex }) {
 
     const initial = -posterWidth * visiblePosterCount;
     const lastAllowedUnclonedPoster = unclonedMoviesCount * -posterWidth;
-    // const lastAllowedPoster = lastAllowedUnclonedPoster + initial;
+    const lastAllowedPoster = lastAllowedUnclonedPoster + initial;
 
     setCanScrollPrev(rowIndex); // makes us able to scroll left after scrolling forward for the first time (just like netflix)
 
@@ -188,7 +191,6 @@ export default function Row({ title, fetchUrl, isLargeRow, rowIndex }) {
             setTranslateXValue(initial);
             timeoutInProgress.current = false;
           }, 750);
-
           setActiveIndex(0);
 
           return translateX;
@@ -289,8 +291,6 @@ export default function Row({ title, fetchUrl, isLargeRow, rowIndex }) {
       </div>
       {canScrollPrev === rowIndex && (
         <button
-          style={{ cursor: timeoutInProgress.current ? 'inherit' : 'pointer' }}
-          disabled={timeoutInProgress.current}
           className="slider__nav prev"
           onClick={() => onNavigate('backward')}
         >
@@ -303,8 +303,6 @@ export default function Row({ title, fetchUrl, isLargeRow, rowIndex }) {
         {CARDS}
       </div>
       <button
-        style={{ cursor: timeoutInProgress.current ? 'inherit' : 'pointer' }}
-        disabled={timeoutInProgress.current}
         className="slider__nav next"
         onClick={() => onNavigate('forward')}
       >
